4ac1ac4ca0b5ae88960c742168319e8a1bd468a1
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-11-10 20:50:36 +0100

    fuck classes, just put the shit in

--- amongus.py
+++ amongus.py
@@ -6,106 +6,98 @@ turned sus by: Omicron166
 A module that converts string to amongus code and vice versa
 
 '''
-class Amongus:
-    def __init__(self, words):
-        '''
-        Takes in one parameter either amongus code or letter and assings it to self.words
-        '''
-        self.codes = {
-            # . -> amongus
-            # - -> sus
-            # ' ' -> sussy-baka
-            "a": "amongus-sus",
-            "b": "sus-amongus-amongus-amongus",
-            "c": "sus-amongus-sus-amongus",
-            "d": "sus-amongus-amongus",
-            "e": "amongus",
-            "f": "amongus-amongus-sus-amongus",
-            "g": "sus-sus-amongus",
-            "h": "amongus-amongus-amongus-amongus",
-            "i": "amongus-amongus",
-            "j": "amongus-sus-sus-sus",
-            "k": "sus-amongus-sus",
-            "l": "amongus-sus-amongus-amongus",
-            "m": "sus-sus",
-            "n": "sus-amongus",
-            "o": "sus-sus-sus",
-            "p": "amongus-sus-sus-amongus",
-            "q": "sus-sus-amongus-sus",
-            "r": "amongus-sus-amongus",
-            "s": "amongus-amongus-amongus",
-            "t": "sus",
-            "u": "amongus-amongus-sus",
-            "v": "amongus-amongus-amongus-sus",
-            "w": "amongus-sus-sus",
-            "x": "sus-amongus-amongus-sus",
-            "y": "sus-amongus-sus-sus",
-            "z": "sus-sus-amongus-amongus",
-            "0": "sus-sus-sus-sus-sus",
-            "1": "amongus-sus-sus-sus-sus",
-            "2": "amongus-amongus-sus-sus-sus",
-            "3": "amongus-amongus-amongus-sus-sus",
-            "4": "amongus-amongus-amongus-amongus-sus",
-            "5": "amongus-amongus-amongus-amongus-amongus",
-            "6": "sus-amongus-amongus-amongus-amongus",
-            "7": "sus-sus-amongus-amongus-amongus",
-            "8": "sus-sus-sus-amongus-amongus",
-            "9": "sus-sus-sus-sus-amongus",
-            ".": "amongus-sus-amongus-sus-amongus-sus",
-            ",": "sus-sus-amongus-amongus-sus-sus",
-            "?": "amongus-amongus-sus-sus-amongus-amongus",
-            "!": "sus-amongus-sus-amongus-sus-sus",
-            "-": "sus-amongus-amongus-amongus-amongus-sus",
-            "/": "sus-amongus-amongus-sus-amongus",
-            "@": "amongus-sus-sus-amongus-sus-amongus",
-            "(": "sus-amongus-sus-sus-amongus",
-            ")": "sus-amongus-sus-sus-amongus-sus",
-            " ": "sussy-baka"
-        }
-        self.words = words
 
-    def ToAmongus(self):
-        '''
-        str -> str
-        returns amongus code variant of the string
+codec = {
+    # . -> amongus
+    # - -> sus
+    # ' ' -> sussy-baka
+    "a": "amongus-sus",
+    "b": "sus-amongus-amongus-amongus",
+    "c": "sus-amongus-sus-amongus",
+    "d": "sus-amongus-amongus",
+    "e": "amongus",
+    "f": "amongus-amongus-sus-amongus",
+    "g": "sus-sus-amongus",
+    "h": "amongus-amongus-amongus-amongus",
+    "i": "amongus-amongus",
+    "j": "amongus-sus-sus-sus",
+    "k": "sus-amongus-sus",
+    "l": "amongus-sus-amongus-amongus",
+    "m": "sus-sus",
+    "n": "sus-amongus",
+    "o": "sus-sus-sus",
+    "p": "amongus-sus-sus-amongus",
+    "q": "sus-sus-amongus-sus",
+    "r": "amongus-sus-amongus",
+    "s": "amongus-amongus-amongus",
+    "t": "sus",
+    "u": "amongus-amongus-sus",
+    "v": "amongus-amongus-amongus-sus",
+    "w": "amongus-sus-sus",
+    "x": "sus-amongus-amongus-sus",
+    "y": "sus-amongus-sus-sus",
+    "z": "sus-sus-amongus-amongus",
+    "0": "sus-sus-sus-sus-sus",
+    "1": "amongus-sus-sus-sus-sus",
+    "2": "amongus-amongus-sus-sus-sus",
+    "3": "amongus-amongus-amongus-sus-sus",
+    "4": "amongus-amongus-amongus-amongus-sus",
+    "5": "amongus-amongus-amongus-amongus-amongus",
+    "6": "sus-amongus-amongus-amongus-amongus",
+    "7": "sus-sus-amongus-amongus-amongus",
+    "8": "sus-sus-sus-amongus-amongus",
+    "9": "sus-sus-sus-sus-amongus",
+    ".": "amongus-sus-amongus-sus-amongus-sus",
+    ",": "sus-sus-amongus-amongus-sus-sus",
+    "?": "amongus-amongus-sus-sus-amongus-amongus",
+    "!": "sus-amongus-sus-amongus-sus-sus",
+    "-": "sus-amongus-amongus-amongus-amongus-sus",
+    "/": "sus-amongus-amongus-sus-amongus",
+    "@": "amongus-sus-sus-amongus-sus-amongus",
+    "(": "sus-amongus-sus-sus-amongus",
+    ")": "sus-amongus-sus-sus-amongus-sus",
+    " ": "sussy-baka"
+}
 
-        i am butterfly
-        >>> amongus-amongus sussy-baka amongus-sus sus-sus sussy-baka sus-amongus-amongus-amongus amongus-amongus-sus sus sus amongus amongus-sus-amongus amongus-amongus-sus-amongus amongus-sus-amongus-amongus sus-amongus-sus-sus
+def encode(input: str) -> str:
+    '''
+    returns amongus code variant of the string
 
-        Cc Aa
-        >>> sus-amongus-sus-amongus sus-amongus-sus-amongus sussy-baka amongus-sus amongus-sus
-        '''
+    i am butterfly
+    >>> amongus-amongus sussy-baka amongus-sus sus-sus sussy-baka sus-amongus-amongus-amongus amongus-amongus-sus sus sus amongus amongus-sus-amongus amongus-amongus-sus-amongus amongus-sus-amongus-amongus sus-amongus-sus-sus
 
-        letters = list(self.words)
-        output = ""
+    Cc Aa
+    >>> sus-amongus-sus-amongus sus-amongus-sus-amongus sussy-baka amongus-sus amongus-sus
+    '''
+
+    letters = list(input)
+    output = ""
+    for letter in letters:
+        if letter.lower() in codec:
+            output += codec[letter.lower()] + " "
+        else:
+            return KeyError
+    return output
+
+def decode(input: str) -> str:
+    '''
+    returns the decoded string as amongus code
+
+    amongus-sus amongus-sus-sus-amongus amongus-sus-sus-amongus amongus-sus-amongus-amongus amongus
+    >>> apple
+
+    amongus-amongus sussy-baka amongus-sus sus-sus sussy-baka amongus-sus sussy-baka sus-amongus-amongus amongus-amongus amongus-amongus-amongus sus-amongus-sus-amongus sus-sus-sus sussy-baka sus-amongus-amongus amongus-sus sus-amongus sus-amongus-sus-amongus amongus amongus-sus-amongus
+    >>> i am a disco dancer
+    '''
+    
+    words =  input.split("  ")
+    output = ""
+    
+    for word in words:
+        letters  = word.split()
         for letter in letters:
-            if letter.lower() in self.codes:
-                output += self.codes[letter.lower()] + " "
-            else:
-                return KeyError
-        return output
-
-    def ToString(self):
-        '''
-        str -> str
-        returns the decoded string as amongus code
-
-        amongus-sus amongus-sus-sus-amongus amongus-sus-sus-amongus amongus-sus-amongus-amongus amongus
-        >>> apple
-
-        amongus-amongus sussy-baka amongus-sus sus-sus sussy-baka amongus-sus sussy-baka sus-amongus-amongus amongus-amongus amongus-amongus-amongus sus-amongus-sus-amongus sus-sus-sus sussy-baka sus-amongus-amongus amongus-sus sus-amongus sus-amongus-sus-amongus amongus amongus-sus-amongus
-        >>> i am a disco dancer
-        '''
-        
-        words =  self.words.split("  ")
-        output = ""
-        
-        for word in words:
-            letters  = word.split()
-            for letter in letters:
-                for string, code in self.codes.items():  # for name, age in dictionary.iteritems():  (for Python 2.x)
-                    if letter == code:
-                        output += string
-            output += " "
-        return output[:-1]
-
+            for string, code in codec.items():  # for name, age in dictionary.iteritems():  (for Python 2.x)
+                if letter == code:
+                    output += string
+        output += " "
+    return output[:-1]
